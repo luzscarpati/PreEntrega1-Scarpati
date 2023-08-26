461f4d94a85a40aaf47468d7cb7c2b8e79e0122e
Luz Scarpati <luz.scarpati@gmail.com>
2023-08-26 18:46:44 -0300

    Creación del archivo services y modificaciones en componentes

--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -1,10 +1,26 @@
+import propTypes from 'prop-types';
 
-const ItemList = () => {
+const ItemList = ({list}) => {
     return (
         <div>
             <h2>ItemList</h2>
+            <ul>
+                {list.map((item) => (
+                    <li key={item.id}>
+                        <h3>{item.name}</h3>
+                        <img src={item.img} alt={item.name}></img>
+                        <p>Precio: ${item.price}</p>
+                        <p>Categoría: {item.category}</p>
+
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
+
+ItemList.propTypes = {
+    list: propTypes.array.isRequired,
+}
 
 export default ItemList;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from "react";
+import { getProducts } from "../../services";
 import ItemList from "./ItemList";
 
 const ItemListContainer = ({greeting}) => {
+  const [list, setList] = useState([]);
+
+  useEffect(() =>{
+      getProducts().then((response) =>{
+        setList(response);
+      });
+  }, [])
+
  return (
     <div>
       <h2 className="text-center">{greeting}</h2>
-      <ItemList/>
+      <ItemList list={list} />
     </div> 
   )   
 };
