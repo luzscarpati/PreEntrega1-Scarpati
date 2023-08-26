709f368f60893697eb708b09112c0e1b18b02ecc
Luz Scarpati <luz.scarpati@gmail.com>
2023-08-26 17:43:23 -0300

    Instalación de propTypes y eliminación de imágenes sobrantes

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,10 @@
+import ItemList from "./ItemList";
+
 const ItemListContainer = ({greeting}) => {
  return (
     <div>
       <h2 className="text-center">{greeting}</h2>
+      <ItemList/>
     </div> 
   )   
 };
