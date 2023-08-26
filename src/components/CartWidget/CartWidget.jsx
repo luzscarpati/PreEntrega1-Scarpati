f0d4ca9a4a5280aab64d18c3a14011615e589cb1
Luz Scarpati <luz.scarpati@gmail.com>
2023-08-26 13:24:47 -0300

    Corrección de rutas de imágenes e incorporación de Link y NavLink en el nav

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,10 @@
+import cart from './cartWidgetImg/shoppingBag.png';
+
 const CartWidget = () => {
   return (
     <button className="btn btn-outline">
       <i>
-      <img src="./src/components/CartWidget/CartWidgetImg/shoppingBag.png" alt="Cart"  width="60" height="54" />
+      <img src={cart} alt="Cart"  width="60" height="54" />
       </i>
       <span className="badge text-bg-secondary">1</span>
       <span className="visually-hidden">Productos en carrito</span>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,25 @@
 import CartWidget from '../CartWidget/CartWidget';
+import {NavLink, Link} from 'react-router-dom';
+import logo from '../../assets/img/butterfly.png';
+
 
 const NavBar = () => {
  return (
   <nav className='navbar navbar-expand-lg bg-body-tertiary'>
   <div className='container-fluid'>
-   <a className="navbar-brand navbar-nav" href="#">
-    <img src="./src/assets/img/butterfly.png" alt="Logo La Tienda" width="60" height="54"/>
-    <h1>La Tienda</h1>
-   </a>
+    <Link to="/" className="navbar-brand navbar-nav">
+      <img src={logo} alt="Logo La Tienda" width="60" height="54"/>
+      <h1>La Tienda</h1>
+    </Link>
   <ul className='navbar-nav'>
     <li className='nav-item'>
-     <a href='#' className='nav-link'>Pantallas</a>
+      <NavLink to="/category/screens" className='nav-link'>Pantallas</NavLink>
     </li>
     <li className='nav-item'>
-     <a href='#' className='nav-link'>Accesorios</a>
+    <NavLink to="/category/accessories" className='nav-link'>Accesorios</NavLink>
     </li>
     <li className='nav-item'>
-     <a href='#' className='nav-link'>Compus y Consolas</a>
+    <NavLink to="/category/pcs" className='nav-link'>Compus y Consolas</NavLink>
     </li>
   </ul>
   <CartWidget/>
